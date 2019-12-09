38978b0ad8a11a19e55d5ecc61d41129a5bc14e4
katifa86 <katifa86@gmail.com>
2019-12-09 13:50:25 +0500

    wrong link check

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,17 @@
-class BasePage():
-	def __init__(self, browser, url):
-		self.browser=browser
-		self.url=url
+from selenium.common.exceptions import NoSuchElementException
 
-	def open(self):
-		self.browser.get(self.url)
+class BasePage():
+    def __init__(self, browser, url, timeout=10):
+        self.browser=browser
+        self.url=url
+        self.browser.implicitly_wait(timeout)
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
