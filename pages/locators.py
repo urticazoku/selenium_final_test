2f9deaa83537e687cb1f03bd41162cabebb259b5
katifa86 <katifa86@gmail.com>
2019-12-11 16:03:52 +0500

    move methods to base page

--- pages/locators.py
+++ pages/locators.py
@@ -1,7 +1,8 @@
 from selenium.webdriver.common.by import By
 
-class MainPageLocators():
-	LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
 class LoginPageLocators():
 	LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")	
@@ -13,4 +14,4 @@ class ProductPageLocators():
 	PRICE = (By.CSS_SELECTOR, '.product_main .price_color')
 	MSG = (By.CSS_SELECTOR, '.alertinner>strong')
 	MSG_PRICE = (By.CSS_SELECTOR, '.alertinner p>strong')
-	
+	SUCCESS_MESSAGE = (By.CSS_SELECTOR, '.alertinner>strong')
--- pages/product_page.py
+++ pages/product_page.py
@@ -2,18 +2,24 @@ from .base_page import BasePage
 from .locators import ProductPageLocators
 
 class ProductPage(BasePage):
-	def add_to_cart(self):
-		link = self.browser.find_element(*ProductPageLocators.BUTTON)
-		link.click()
+    def add_to_cart(self):
+        link = self.browser.find_element(*ProductPageLocators.BUTTON)
+        link.click()
 
-	def get_title(self):
-		return self.browser.find_element(*ProductPageLocators.TITLE).text
+    def get_title(self):
+        return self.browser.find_element(*ProductPageLocators.TITLE).text
 
-	def get_price(self):
-		return self.browser.find_element(*ProductPageLocators.PRICE).text
+    def get_price(self):
+        return self.browser.find_element(*ProductPageLocators.PRICE).text
 
-	def should_add_to_basket(self, title, price):
-		msg_title = self.browser.find_element(*ProductPageLocators.MSG)
-		msg_price = self.browser.find_element(*ProductPageLocators.MSG_PRICE)
-		assert title==msg_title.text, "{} not in {}".format(title,msg.text)
-		assert price==msg_price.text, "{} not in {}".format(price,msg.text)
+    def should_add_to_basket(self, title, price):
+        msg_title = self.browser.find_element(*ProductPageLocators.MSG)
+        msg_price = self.browser.find_element(*ProductPageLocators.MSG_PRICE)
+        assert title==msg_title.text, "{} not in {}".format(title,msg.text)
+        assert price==msg_price.text, "{} not in {}".format(price,msg.text)
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, but should not be"
+
+    def should_disappear(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), "Not dissapeared, but should not be"
